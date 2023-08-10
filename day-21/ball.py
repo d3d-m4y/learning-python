4f77829072ab373a05c53f6be9879cc2dfe8e057
d3d-m4y <dave@davemobley.net>
2023-08-09 19:39:16 -0600

    basically done.

--- day-21/ball.py
+++ day-21/ball.py
@@ -7,7 +7,16 @@ class Ball(Turtle):
         super().__init__()
         self.color("white")
         self.shape("circle")
+        self.x_move = 20
+        self.y_move = 20
 
     def move(self):
-        self.setheading(33.75)
-        self.forward(20)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(x=new_x, y=new_y)
+
+    def wall_bounce(self):
+        self.y_move *= -1
+
+    def paddle_bounce(self):
+        self.x_move *= -1
